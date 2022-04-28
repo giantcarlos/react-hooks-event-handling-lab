b4f920e9e10b3a2942801bb31f8174a2682e8d20
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-04-28 14:46:19 -0500

    commit tree 94d8b0a8dd0def2d60429328e6f721dd6ce8cb1c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function handleFocus(event) {
+        if (event.type === 'blur'){
+            console.log('Hey! Eyes on me!')
+        }
+        else if (event.type === 'focus'){
+            console.log('Good!')
+        }
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleFocus}>Eyes on me!</button>
+    )
+}
+
+export default EyesOnMe;
